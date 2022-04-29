b35a4f06d3ca7f5ff62874fe4f663878e7cf0484
Giang <gianglt@gmail.com>
2022-04-29 10:41:53 +0700

    Saved

--- examples/RNOneSignalTS/src/OSDemo.tsx
+++ examples/RNOneSignalTS/src/OSDemo.tsx
@@ -35,7 +35,7 @@ class OSDemo extends React.Component<Props, State> {
 
     async componentDidMount() {
         /* O N E S I G N A L   S E T U P */
-        OneSignal.setAppId("ce8572ae-ff57-4e77-a265-5c91f00ecc4c");
+        OneSignal.setAppId("2f141577-4ca2-4634-bdda-2205aa9d71b2");
         OneSignal.setLogLevel(6, 0);
         OneSignal.setRequiresUserPrivacyConsent(this.state.requiresPrivacyConsent);
         OneSignal.promptForPushNotificationsWithUserResponse(response => {
